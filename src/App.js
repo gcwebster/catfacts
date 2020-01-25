d6e9c2d86ca2b5b1892a7d7d54bba3c583997588
George Webster <gcwebster@hotmail.co.uk>
2020-01-25 22:46:57 +0000

    basics of state working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Cat facts</h1>
-      <p>test</p>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { fact: "" };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Cat facts</h1>
+        <p>
+          Welcome to catfacts, click below to find out a new fact about cats
+        </p>
+        <button onClick={this.newFact}>New catfact</button>
+        <p>{this.state.fact}</p>
+      </div>
+    );
+  }
+
+  newFact = () => {
+    this.setState({
+      fact: "cat's like mice"
+    });
+  };
 }
 
 export default App;
